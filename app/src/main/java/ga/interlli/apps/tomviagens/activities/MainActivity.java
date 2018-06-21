cde2bba690205bb5e6159a293928568f847463ed
Lincoln Lamas <llamas.jf@gmail.com>
2018-06-21 02:56:39 -0300

    Tela inicial

--- app/src/main/java/ga/interlli/apps/tomviagens/activities/MainActivity.java
+++ app/src/main/java/ga/interlli/apps/tomviagens/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package ga.interlli.apps.tomviagens;
+package ga.interlli.apps.tomviagens.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ga.interlli.apps.tomviagens.R;
 
 public class MainActivity extends AppCompatActivity {
 
